d3c0b1569aa6ba85024237a94b31d25d33937fb0
DmitriyGovosyants <Demiangvs@gmail.com>
2022-07-14 12:37:14 +0300

    initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import { Container } from 'components';
+import { Section } from './App.styled';
+
+export class App extends Component {
+  render() {
+    return (
+      <Section>
+        <Container></Container>
+      </Section>
+    );
+  }
+}
